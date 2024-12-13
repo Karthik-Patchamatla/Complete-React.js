be627f5b9d30c04d5078be042f2071757366f07e
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-12-13 15:29:59 +0530

    TailwindCSS

--- React-Learning/src/App.jsx
+++ React-Learning/src/App.jsx
@@ -1,16 +1,8 @@
-import React, { useState, useEffect } from 'react';
-
 function App() {
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        document.title = `Count: ${count}`;
-    }, [count]);
-
-    return (
-        <div>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+    return(
+        <div className="container">
+            <h1>React Learning</h1>
+            <p>Learning React</p>
         </div>
     );
 }
